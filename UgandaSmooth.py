02b85cbd8118978ff56d5c78840041dc8544aad7
Angela Cai <angelacai3339@gmail.com>
2024-06-24 23:26:01 -0400

    Add files via upload

--- UgandaSmooth.py
+++ UgandaSmooth.py
@@ -2,17 +2,22 @@ import rioxarray
 import xarray
 from scipy.ndimage import generic_filter
 
+# Function for generic_filter
 def mean_filter(window):
     return window.mean()
 
-covariate = rioxarray.open_rasterio("/Users/angelacai/TKI/Standardized Uganda/standard_uganda_Rainfall_CHIRPS_2km_2018.tif")
+# Array for generic_filter
+covariate = rioxarray.open_rasterio("/Users/angelacai/TKI/Standardized Uganda/standard_uganda_elevation.tif")
 covariate_values = covariate.to_numpy()
 
+# Convert raster distance (degrees) to pixel size for generic_filter
+DEGREES_PER_KM = 111
 resolution = covariate.rio.resolution()[0]
-smooth_distances = [.1, .22, 1.02] # in meters
+smooth_distances = [.001, .0022, .0102] # in km
 
+# Apply smoothing
 for distance in smooth_distances:
-    smooth_window = int(distance / resolution)
+    smooth_window = int(distance * DEGREES_PER_KM / resolution)
     smoothed = generic_filter(covariate_values, mean_filter, size=smooth_window)
     smoothed_data_xr = xarray.DataArray(smoothed, dims=covariate.dims, coords=covariate.coords, attrs=covariate.attrs)
-    smoothed_data_xr.rio.to_raster("Smoothed Uganda/smooth_uganda_rain_" + str(distance * 100) + "cm_2018.tif")
+    smoothed_data_xr.rio.to_raster("Smoothed Uganda/smooth_uganda_elevation_" + str(distance * 1000) + "m_2018.tif")
