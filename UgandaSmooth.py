39594c25953aedaea11894f96613394d471396f2
Angela Cai <angcai@princeton.edu>
2024-06-25 12:30:19 +0800

    readablility

--- UgandaSmooth.py
+++ UgandaSmooth.py
@@ -11,10 +11,11 @@ covariate = rioxarray.open_rasterio("Uganda Covariate Rasters/elevation.tif")
 covariate_values = covariate.to_numpy()
 
 # Convert raster distance (degrees) to pixel size for generic_filter
-smooth_distances = [5, 11, 51] # in km
+KM_PER_PIXEL = 2
+smooth_distances = [5, 11, 51] # in pixels
 
 # Apply smoothing
 for distance in smooth_distances:
     smoothed = generic_filter(covariate_values, mean_filter, size=distance)
     smoothed_data_xr = xarray.DataArray(smoothed, dims=covariate.dims, coords=covariate.coords, attrs=covariate.attrs)
-    smoothed_data_xr.rio.to_raster("Uganda Smoothed Rasters/smooth_uganda_elevation_" + str(distance*2) + "km_2018.tif")
+    smoothed_data_xr.rio.to_raster("Uganda Smoothed Rasters/smooth_uganda_elevation_" + str(distance*KM_PER_PIXEL) + "km_2018.tif")
