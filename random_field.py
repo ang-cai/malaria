82c234ae69a5e1ed3903f3e8fd176016fdf96d9d
Angela Cai <angcai@princeton.edu>
2024-06-26 22:34:33 +0800

    idk why smaller length scale is making more blurry

--- random_field.py
+++ random_field.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import rioxarray
+import xarray
 import gpflow
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
@@ -22,25 +23,26 @@ if __name__ == "__main__":
     X = np.stack((x, y, lst, rain, elevation), axis = -1)
 
     # Create train points and test grid
-    X_train, X_test, Z_train, Z_test = train_test_split(X, Z, test_size=0.95)
+    X_train, X_test, Z_train, Z_test = train_test_split(X, Z, test_size=0.9)
     malaria_prevalence = rioxarray.open_rasterio("Uganda Malaria Data/mock_malaria_prevelance_uganda_2km_2018.tif").squeeze()
     left, bottom, right, top = malaria_prevalence.rio.bounds()
     x = np.linspace(left, right, len(X_test))
-    print(len(X_test))
     y = np.linspace(bottom, top, len(X_test))
     x1, x2 = np.meshgrid(x, y)
     X_grid = np.stack((x1.ravel(), x2.ravel()), axis = -1)
-    covariates = ["LSTday", "Rainfall_CHIRPS", "elevation"]
+    covariates = ["LSTday_2km_2018", "Rainfall_CHIRPS_2km_2018", "elevation_2km"]
+    x_points = xarray.DataArray(X_grid[:, 0])
+    y_points = xarray.DataArray(X_grid[:, 1])
     for covariate in covariates:
-        src = rioxarray.open_rasterio("Uganda Standardized Rasters/standard_uganda_" + covariate + "_2km_2018.tif")
-        data = src.sel(x=X_grid[:, 0], y=X_grid[:, 1], method="nearest")
-        X_grid = np.concatenate((X_grid, data.values[0][0]), axis = -1)
+        src = rioxarray.open_rasterio("Uganda Standardized Rasters/standard_uganda_" + covariate + ".tif")
+        data = src.sel(x=x_points, y=y_points, method="nearest")
+        X_grid = np.concatenate((X_grid, data.to_numpy().reshape(-1, 1)), axis = -1)
 
-    # Different r's in graphic
+    # Different lengthscales in graphic
     lengthscales = [5, 1, .5, .1, .05]
     
     # Initialize graphic
-    figure, axis = plt.subplots(1, len(lengthscales), figsize=(15, 6))
+    figure, axis = plt.subplots(1, len(lengthscales), figsize=(30, 6), sharey=True)
 
     for i in range(len(lengthscales)):
         # Kernal is made of RBF acting on the coordinates and linear acting on the covariates
@@ -62,7 +64,10 @@ if __name__ == "__main__":
 
         # Add figure to graphic
         axis[i].contourf(x, y, Z_fmean_grid, levels=100, cmap='viridis')
-        plt.title("GLS: r=" + str(lengthscales[i]))
+        axis[i].get_xaxis().set_visible(False)
+        axis[i].get_yaxis().set_visible(False)
+        figure.subplots_adjust(wspace=0)
+        axis[i].set_title("GLS: length scale=" + str(lengthscales[i]))
 
-    plt.savefig("Uganda Malaria Data/mock_malaria_cases_uganda_randomfield_2km_2018.png", bbox_inches="tight") 
+    plt.savefig("Uganda Malaria Data/mock_malaria_cases_uganda_randomfield_2km_2018.png", bbox_inches="tight", pad_inches=0) 
     plt.show()
