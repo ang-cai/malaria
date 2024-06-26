78bfdca9de02120c0768a278912bfb8d58068177
Angela Cai <angcai@princeton.edu>
2024-06-26 20:20:47 +0800

    comments

--- correlation.py
+++ correlation.py
@@ -5,23 +5,27 @@ import matplotlib.pyplot as plt
 # Returns a graphic of a correlation matrix given covariates
 if __name__ == "__main__":
     # Covariates
-    lst = rioxarray.open_rasterio("Uganda Standardized Rastors/standard_uganda_LSTday_2km_2018.tif")
-    rainfall = rioxarray.open_rasterio("Uganda Standardized Rastors/standard_uganda_Rainfall_CHIRPS_2km_2018.tif")
-    elevation= rioxarray.open_rasterio("Uganda Standardized Rastors/standard_uganda_elevation.tif")
+    lst = rioxarray.open_rasterio("Uganda Standardized Rasters/standard_uganda_LSTday_2km_2018.tif")
+    rainfall = rioxarray.open_rasterio("Uganda Standardized Rasters/standard_uganda_Rainfall_CHIRPS_2km_2018.tif")
+    elevation= rioxarray.open_rasterio("Uganda Standardized Rasters/standard_uganda_elevation_2km.tif")
 
     # Different smoothing graphics
-    distances = [10, 22, 102] # in km
+    distances = [0, 102, 22, 10] # in km
+
+    # Initialize graphic
+    figure, axis = plt.subplots(1, len(distances), figsize=(15, 6))
 
     # Subtract smooth from raw and then create a correlation graphic between covariates
-    for distance in distances:
-        # Subtract smooth
-        lst_smooth = rioxarray.open_rasterio("Uganda Smoothed Rasters/smooth_uganda_lst_" + str(distance) + "km_2018.tif")
-        rainfall_smooth = rioxarray.open_rasterio("Uganda Smoothed Rasters/smooth_uganda_rain_" + str(distance) + "km_2018.tif")
-        elevation_smooth = rioxarray.open_rasterio("Uganda Smoothed Rasters/smooth_uganda_elevation_" + str(distance) + "km_2018.tif")
-        
-        lst = lst - lst_smooth
-        rainfall = rainfall - rainfall_smooth
-        elevation = elevation - elevation_smooth
+    for i in range(len(distances)):
+        if i > 0:
+            # Subtract smooth
+            lst_smooth = rioxarray.open_rasterio("Uganda Smoothed Rasters/smooth_uganda_lst_" + str(distances[i]) + "km_2018.tif")
+            rainfall_smooth = rioxarray.open_rasterio("Uganda Smoothed Rasters/smooth_uganda_rain_" + str(distances[i]) + "km_2018.tif")
+            elevation_smooth = rioxarray.open_rasterio("Uganda Smoothed Rasters/smooth_uganda_elevation_" + str(distances[i]) + "km_2018.tif")
+            
+            lst = lst - lst_smooth
+            rainfall = rainfall - rainfall_smooth
+            elevation = elevation - elevation_smooth
 
         lst_values = lst.to_numpy().flatten()
         rainfall_values = rainfall.to_numpy().flatten()
@@ -33,14 +37,18 @@ if __name__ == "__main__":
         pretty_correlation_matrix = numpy.absolute(numpy.rot90(correlation_matrix))
 
         # Plot the correlation matrix
-        fig, ax = plt.subplots(figsize=(6, 6))
-        cax = ax.matshow(pretty_correlation_matrix, cmap="viridis")
-        ax.set_xticks([0, 1, 2])
-        ax.set_yticks([0, 1, 2])
-        ax.set_xticklabels(["LST", "Rainfall", "Elevation"], rotation=90, fontsize=16)
-        ax.set_yticklabels(["Elevation", "Rainfall", "LST"], fontsize=16)
-        ax.xaxis.set_ticks_position('bottom')
-        ax.xaxis.set_label_position('bottom')
-        plt.title(str(-distance) + "m smooth", fontsize=20)
-        plt.tight_layout()
-        plt.savefig("Uganda Malaria Data/" + str(distance) + "km_covariates_correlation_uganda_2km_2018.png") 
+        axis[i].matshow(pretty_correlation_matrix, cmap="viridis")
+        axis[i].set_xticks([0, 1, 2])
+        axis[i].set_yticks([0, 1, 2])
+        axis[i].set_xticklabels(["LST", "Rainfall", "Elevation"], rotation=90, fontsize=16)
+        axis[i].set_yticklabels(["Elevation", "Rainfall", "LST"], fontsize=16)
+        axis[i].xaxis.set_ticks_position('bottom')
+        axis[i].xaxis.set_label_position('bottom')
+        if i > 0:
+            axis[i].set_title(str(-distances[i]) + "m smooth", fontsize=20)
+        else:
+            axis[i].set_title("Raw Data", fontsize=20)
+    
+    # Save graphic
+    plt.tight_layout()
+    plt.savefig("Uganda Malaria Data/covariates_correlation_uganda_2km_2018.png", bbox_inches="tight") 
--- random_field.py
+++ random_field.py
@@ -1,85 +1,68 @@
 import pandas as pd
 import numpy as np
 import rioxarray
-import rasterio
-
-import matplotlib.pyplot as plt
-
 import gpflow
 import tensorflow as tf
-
 from sklearn.model_selection import train_test_split
-uganda_data = pd.read_csv("Uganda Malaria Data/mock_malaria_cases_uganda_2km_2018.csv")
-Z = uganda_data["malaria"].to_numpy().reshape(-1, 1)
-x = uganda_data["x"].to_numpy()
-y = uganda_data["y"].to_numpy()
-lst = uganda_data["lst"].to_numpy()
-rain = uganda_data["rainfall"].to_numpy()
-elevation = uganda_data["elevation"].to_numpy()
-X = np.stack((x, y, lst, rain, elevation), axis = -1)
-rbf = gpflow.kernels.RBF(active_dims = [0, 1], lengthscales=1.0)
-linear = gpflow.kernels.Linear(active_dims = [2, 3, 4])
-kernel = rbf + linear
-X_train, X_test, Y_train, Y_test = train_test_split(X, Z, test_size=0.95)
-model = gpflow.models.GPR(data = (X_train, Y_train), kernel = kernel)
-opt = gpflow.optimizers.Scipy()
-opt.minimize(model.training_loss, model.trainable_variables)
-malaria_prevalence = rioxarray.open_rasterio("Uganda Malaria Data/mock_malaria_prevelance_uganda_2km_2018.tif").squeeze()
-left, bottom, right, top = malaria_prevalence.rio.bounds()
-x = np.linspace(left, right, 285)
-y = np.linspace(bottom, top, 285)
-x1, x2 = np.meshgrid(x, y)
-X_grid = np.stack((x1.ravel(), x2.ravel()), axis = -1)
-with rasterio.open("Uganda Standardized Rasters/standard_uganda_LSTday_2km_2018.tif") as src:
-    # Initialize an empty list to store the sampled values
-    values = []
+import matplotlib.pyplot as plt
+
+# Returns a raster of Malaria prevalence using a linear function on the covariates plus a random field function
+if __name__ == "__main__":
+    # Load in Malaria data
+    tf.experimental.numpy.experimental_enable_numpy_behavior()
+    uganda_data = pd.read_csv("Uganda Malaria Data/mock_malaria_cases_uganda_2km_2018.csv")
     
-    # Sample the raster at the points
-    for point in X_grid:
-        # Sample the raster at each point
-        for val in src.sample([point.tolist()]):
-            values.append(val[0])
+    # numpy requires column vectors
+    Z = uganda_data["malaria"].to_numpy().reshape(-1, 1)
+    x = uganda_data["x"].to_numpy()
+    y = uganda_data["y"].to_numpy()
+    lst = uganda_data["lst"].to_numpy()
+    rain = uganda_data["rainfall"].to_numpy()
+    elevation = uganda_data["elevation"].to_numpy()
+    X = np.stack((x, y, lst, rain, elevation), axis = -1)
 
-# Convert the list of values to a numpy array
-lst_data = np.array(values)
-with rasterio.open("Uganda Standardized Rasters/standard_uganda_Rainfall_CHIRPS_2km_2018.tif") as src:
-    # Initialize an empty list to store the sampled values
-    values = []
+    # Create train points and test grid
+    X_train, X_test, Z_train, Z_test = train_test_split(X, Z, test_size=0.95)
+    malaria_prevalence = rioxarray.open_rasterio("Uganda Malaria Data/mock_malaria_prevelance_uganda_2km_2018.tif").squeeze()
+    left, bottom, right, top = malaria_prevalence.rio.bounds()
+    x = np.linspace(left, right, len(X_test))
+    print(len(X_test))
+    y = np.linspace(bottom, top, len(X_test))
+    x1, x2 = np.meshgrid(x, y)
+    X_grid = np.stack((x1.ravel(), x2.ravel()), axis = -1)
+    covariates = ["LSTday", "Rainfall_CHIRPS", "elevation"]
+    for covariate in covariates:
+        src = rioxarray.open_rasterio("Uganda Standardized Rasters/standard_uganda_" + covariate + "_2km_2018.tif")
+        data = src.sel(x=X_grid[:, 0], y=X_grid[:, 1], method="nearest")
+        X_grid = np.concatenate((X_grid, data.values[0][0]), axis = -1)
+
+    # Different r's in graphic
+    lengthscales = [5, 1, .5, .1, .05]
     
-    # Sample the raster at the points
-    for point in X_grid:
-        # Sample the raster at each point
-        for val in src.sample([point.tolist()]):
-            values.append(val[0])
+    # Initialize graphic
+    figure, axis = plt.subplots(1, len(lengthscales), figsize=(15, 6))
 
-# Convert the list of values to a numpy array
-rain_data = np.array(values)
-with rasterio.open("Uganda Standardized Rasters/standard_uganda_elevation_2km.tif") as src:
-    # Initialize an empty list to store the sampled values
-    values = []
-    
-    # Sample the raster at the points
-    for point in X_grid:
-        # Sample the raster at each point
-        for val in src.sample([point.tolist()]):
-            values.append(val[0])
+    for i in range(len(lengthscales)):
+        # Kernal is made of RBF acting on the coordinates and linear acting on the covariates
+        rbf = gpflow.kernels.RBF(active_dims = [0, 1], lengthscales=lengthscales[i])
+        linear = gpflow.kernels.Linear(active_dims = [2, 3, 4])
+        kernel = rbf + linear
 
-# Convert the list of values to a numpy array
-elevation_data = np.array(values)
-X_grid = np.concatenate((X_grid, lst_data[:, np.newaxis], rain_data[:, np.newaxis], elevation_data[:, np.newaxis]), axis = -1)
-tf.experimental.numpy.experimental_enable_numpy_behavior()
-# Make predictions on the grid
-Z_fmean_grid, Z_fvar_grid = model.predict_f(X_grid)
+        # Train model
+        model = gpflow.models.GPR(data = (X_train, Z_train), kernel = kernel)
+        opt = gpflow.optimizers.Scipy()
+        opt.minimize(model.training_loss, model.trainable_variables)
 
-# Reshape the grid predictions to match the grid shape
-Z_fmean_grid = Z_fmean_grid.reshape(x1.shape)
-Z_fvar_grid = Z_fvar_grid.reshape(x1.shape)
-plt.figure(figsize=(8, 6))
-plt.contourf(x, y, Z_fmean_grid, levels=100, cmap='viridis')
-plt.colorbar(label='Malaria Prevalence')
+        # Fit model onto coordinate area
+        Z_fmean_grid, Z_fvar_grid = model.predict_f(X_grid)
 
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('GLMM Predicted Malaria Prevalence in Uganda 2018')
-plt.savefig("Uganda Malaria Data/mock_malaria_cases_uganda_randomfield_2km_2018.png") 
-plt.show()
+        # Reshape the grid predictions to match the grid shape
+        Z_fmean_grid = Z_fmean_grid.reshape(x1.shape)
+        Z_fvar_grid = Z_fvar_grid.reshape(x1.shape)
+
+        # Add figure to graphic
+        axis[i].contourf(x, y, Z_fmean_grid, levels=100, cmap='viridis')
+        plt.title("GLS: r=" + str(lengthscales[i]))
+
+    plt.savefig("Uganda Malaria Data/mock_malaria_cases_uganda_randomfield_2km_2018.png", bbox_inches="tight") 
+    plt.show()
